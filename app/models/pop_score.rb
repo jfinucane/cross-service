b4a2cc85c9d062290f3864bdf215b41f09b2fc6a
jimfinucane <jamesfincuane@gmail.com>
2012-11-16 17:52:27 -0500

    pop scores testing

--- app/models/pop_score.rb
+++ app/models/pop_score.rb
@@ -1,3 +1,16 @@
 class PopScore < ActiveRecord::Base
   attr_accessible :dictionary_id, :score, :word
+  def self.create params
+    t = self.find_by_dictionary_id_and_word(params[:dictionary_id], params[:word])
+    if t
+      t.update_attributes(score: params[:score])
+    else
+      p = self.new(params)
+      result = p.save
+    end
+  end
+  def reset dictionary_id
+  	scores = PopScore.where(:dictionary_id => dictionary_id)
+  	scores.each{|s|s.delete}
+  end
 end
--- spec/models/pop_score_spec.rb
+++ spec/models/pop_score_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe PopScore do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+
+  before :all do 
+  	@dict='advancedtest'
+    raise unless @id= Dictionary.find_by_name(@dict).id
+    scores = PopScore.where(:dictionary_id => @id)
+    scores.each{|s| s.delete}
+  end
+  describe '#create' do
+  	before :all do
+  	  @word = 'aardvark'
+  	  p = PopScore.create(word: @word, score: 40, dictionary_id: @id)
+      @q = PopScore.where(word: @word, dictionary_id: @id)
+    end
+    it  'there will be one score for a word' do
+      expect(@q.count).to eql(1)
+    end
+    it 'will update if word exists' do
+      p = PopScore.create(word: @word, score: 60, dictionary_id: @id)
+      @q = PopScore.where(word: @word, dictionary_id: @id)
+      expect(@q.count).to eql(1)
+      expect(@q.first.score).to eql(60)
+    end
+  end
+  describe '#reset' do
+    it 'should reset the scores database' do
+      p = PopScore.create(word: 'abc', score: 40, dictionary_id: @id)
+      p = PopScore.create(word: 'xyz', score: 40, dictionary_id: @id)
+      q = PopScore.where(dictionary_id: @id)
+      q.count.should > 1
+      PopScore.new.reset @id
+      q = PopScore.where(dictionary_id: @id)
+      expect(q.count).to eql(0)
+    end
+  end
 end
