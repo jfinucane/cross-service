b41dbe1a2eff0d37d3765cc301a99f879e42ddd3
jimfinucane <jamesfincuane@gmail.com>
2012-12-11 15:02:37 -0500

    ui width

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def column_count item_count, item_length
   	if item_count < 10
       @cols = 1
-    elsif item_length < 5
+    elsif item_length < 6
       @cols = 6
     elsif item_length < 8
-      @cols =4
+      @cols = 4
     else
       @cols = 2
     end
